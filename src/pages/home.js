13d2bfcdb21a7fee8ea483a5722f157c6ba6ad2f
mlak <manjunathasl@live.com>
2022-01-24 13:30:04 +0530

    products infinite scrolling implementation

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,35 +2,34 @@ import { useState, useEffect } from "react";
 import SearchBox from "../components/SearchBox";
 import { signout, getUser } from "../services/auth";
 import { getProducts } from "../services/products";
-
-import DataTable from "react-data-table-component";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import "./home.css";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
   const [svcError, setSvcError] = useState("");
   const [totalPages, setTotalPages] = useState(0);
 
   const [page, setPage] = useState(1);
-  
-  const [searchStr, setSearchStr] = useState('');
+
+  const [searchStr, setSearchStr] = useState("");
 
   const onSearch = (str) => {
-    setSearchStr(str)
+    setSearchStr(str);
   };
 
   const logout = (e) => {
     e.preventDefault();
     signout();
   };
-  const handlePageChange = (page) =>{
-    if(page < totalPages){
-      setPage(page + 1)
+  const loadMore = () => {
+    if (!loading && page < totalPages) {
+      setPage(page + 1);
     }
-  }
+  };
 
   useEffect(() => {
     if (!user) {
@@ -42,7 +41,7 @@ export default function Home() {
         setLoading(true);
         const data = await getProducts(page);
         if (data.products) {
-          setTotalPages(data.totalPage);
+          setTotalPages(data.totalPages);
           setProducts([...products, ...data.products]);
         }
       } catch (error) {
@@ -73,7 +72,7 @@ export default function Home() {
   return (
     <div className="home">
       <div className="user">
-        <div>{user.userName}</div>
+        <div>{user?.userName}</div>
         <a href="/logout" onClick={logout}>
           Log Out
         </a>
@@ -82,15 +81,31 @@ export default function Home() {
         <SearchBox placeholder="Search for laptop modal" onSearch={onSearch} />
       </div>
       <div className="grid">
-        {svcError && <div className="error-text">{svcError}</div>}
-        <DataTable
-          columns={columns}
-          data={searchStr ? products.filter(it => it.productName.toLowerCase().includes(searchStr.toLowerCase())): products}
-          progressPending={loading}
-          pagination
-          paginationServer
-          onChangePage={handlePageChange}
-        />
+        <div className="row header">
+          <div className="cell">ID</div>
+          <div className="cell">Product Name</div>
+          <div className="cell">Price</div>
+        </div>
+        <InfiniteScroll
+          dataLength={(searchStr ? products.filter(it => it.productName.toLowerCase().includes(searchStr.toLowerCase())): products).length}
+          next={loadMore}
+          hasMore={true}
+          loader={loading ? <h4>Loading...</h4> : ''}
+          height={200}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>{svcError}</b>
+            </p>
+          }
+        >
+          {(searchStr ? products.filter(it => it.productName.toLowerCase().includes(searchStr.toLowerCase())): products).map((item, index) => (
+            <div className="row" key={index}>
+              <div className="cell">{item.id}</div>
+              <div className="cell">{item.productName}</div>
+              <div className="cell">{item.price}</div>
+            </div>
+          ))}
+        </InfiniteScroll>
       </div>
     </div>
   );
